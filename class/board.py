2c337167dc6b8358ec37a1583eb4f568d7fe5571
produy <nguyentrinhduy2003@gmail.com>
2023-11-24 20:28:45 +0700

    initial commit

---
+++ class/board.py
@@ -0,0 +1,8 @@
+# A Board will have the following attributes:
+#     - rows
+#     - cols
+#     - 2D array
+class Board:
+    def __init__(self, rows, cols) -> None:
+        self
+    def 
---
+++ level/level1.py
@@ -0,0 +1,5 @@
+class Level1:
+    def bfs():
+    def dfs():
+    def a_star():
+    def ucs:
